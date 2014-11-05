0353065cd447dcfcaf50c760b9e366e6e21d3c44
Herbert Siojo <herbert.siojo@gmail.com>
2014-11-05 13:18:13 -0800

    Fixed a z-index issue in freelancer.css, where the modal backdrop would obscure the content. Added nokbox images to portfolio. Added another lightbox library, which I might use for video stuff.

--- portfolio_test/js/portfolio.js
+++ portfolio_test/js/portfolio.js
@@ -1,11 +1,6 @@
 /*
-$(document).ready(function () {
-    // console.log('here');
-    $('a.gallery-link').colorbox();
-});
-*/
-
 $(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
     event.preventDefault();
     $(this).ekkoLightbox();
 });
+*/
